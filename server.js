b63c5cdd572274bff188e3493cf635672c6f5532
rimonmath <rimonmath@gmail.com>
2016-06-20 07:44:42 +0000

    aaaaaaaaa

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080; 
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
